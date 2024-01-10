4cad4b74709dd4b847d68f91a251badd79efefba
Radu <raduciurescu75@gmail.com>
2024-01-10 18:44:23 +0200

    !!update!!

--- PROIECT POO/PROIECT POO.cpp
+++ PROIECT POO/PROIECT POO.cpp
@@ -1,16 +1,122 @@
-// PROIECT POO.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// cartile vor fi introduse astfel: Primul cuvant este tipul cartii, fictiune sau nonfictiune, in cazul in care este fictiune dupa tip urmeaza numarul de pagini, in caz contrar anul publicarii.
+//dupa urmeaza titlul scris impreuna, pretul, numarul de carti valabile in stoc iar la final autorii iar dupa ultimul autor . (punct)
+
+
 #include<list>
 #include <iostream>
 #include<fstream>
 #include<string>
+#include<Windows.h>
 using namespace std;
 
+class carte {
+private:
+    string titlu, editura;
+    list<string>nume_autori;
+    float pret;
+    int stock;
+    bool imprumut=false;
+public:
+    carte(string _titlu, list<string> _numele_autorilor, string _editura, float _pret, int _stock)
+    {
+        this->titlu = _titlu;
+        this->nume_autori = _numele_autorilor;
+        this->editura = _editura;
+        this->stock = _stock;
+        this->pret = _pret;
+    }
+
+    virtual float getpret()
+    {
+        return pret;
+    }
+    virtual string tip()
+    {
+        return "nu are";
+    }
+    void diminuare_stoc()
+    {
+        stock =stock- 1;
+    }
+    void seteaza_imprumut()
+    {
+        imprumut = true;
+    }
+    bool checkimprumut()
+    {
+        return imprumut;
+    }
+    virtual void afisare_carte()
+    {
+        cout << titlu <<" sunt in stoc "<<stock<<" exemplare de la editura " << editura << " scrisa de ";
+        for (auto i : nume_autori)
+            cout << i << " ";
+    }
+    string gettitlu()
+    {
+        return titlu;
+    }
+    int getstoc()
+    {
+        return stock;
+    }
+};
+class carte_fictiune : public carte
+{
+private:
+    int nr_pagini;
+public:
+    carte_fictiune(string _titlu, list<string> _numele_autorilor, string _editura, float _pret, int _stock, int _nr_pagini) : carte(_titlu, _numele_autorilor, _editura, _pret, _stock)
+    {
+        this->nr_pagini = _nr_pagini;
+    }
+
+    float getpret()override
+    {
+        return carte::getpret() + nr_pagini;
+    }
+    string tip()override {
+        return "fictiune";
+    }
+    void afisare_carte()override
+    {
+        cout <<"Tip: "<< tip() << " titlu: ";
+        carte::afisare_carte();
+        cout << " costa: " << getpret()<<" lei si are "<<nr_pagini<<" pagini.";
+    }
+};
+class carte_nonfictiune : public carte
+{
+private:
+    int an_publicare;
+public:
+    carte_nonfictiune(string _titlu, list<string> _numele_autorilor, string _editura, float _pret,int _stock, int _an_publicare) :carte(_titlu, _numele_autorilor, _editura, _pret, _stock)
+    {
+        this->an_publicare = _an_publicare;
+    }
+
+    float getpret()override
+    {
+        return carte::getpret() - (25 * an_publicare);
+    }
+    string tip()override {
+        return "nonfictiune";
+    }
+    void afisare_carte()override
+    {
+        cout << "Tip: " << tip() << " titlu: ";
+        carte::afisare_carte();
+        cout << ", costa: " << getpret()<<" lei si a fost publicata in anul "<<an_publicare<<".";
+    }
+};
 class utilizator
 {
 private:
     string nume, prenume, CNP, email, parola;
+    list<carte*> istoric;
+    float cheltuit=0;
 public:
+    
     utilizator(string nume, string prenume, string CNP, string email, string parola)
     {
         this->nume = nume;
@@ -18,6 +124,7 @@ public:
         this->CNP = CNP;
         this->email = email;
         this->parola = parola;
+        this->cheltuit = 0;
     }
     bool checkmail(string email)
     {
@@ -51,7 +158,26 @@ public:
     {
         parola = parola_;
     }
+    void adaugacarteinistoric(carte *carte)
+    {
+        istoric.push_back(carte);  //!
+    }
+    void crestecheltuiala(float suma)
+    {
+        cheltuit = cheltuit + suma; //!
+    }
+    void afisareistoric()
+    {
+        cout << "utilizatorul: " << nume << " a achizitionat urmatoarele carti: \n";
+        for (auto i : istoric)
+        {
+           cout<< i->gettitlu()<<"; ";
+            
+        }
+        cout << "\nTotal cheltuit: " << cheltuit << " lei\n";
+    }
 };
+
 
 utilizator* autentificare(list<utilizator*>utilizatori)
 {
@@ -61,7 +187,7 @@ utilizator* autentificare(list<utilizator*>utilizatori)
         cout << "Introdu parola: "; cin >> parola;
         for (auto i : utilizatori)
         {
-            if (i->checkmail(email) == i->checkparola(parola) == 1)
+            if (i->checkmail(email) == 1 && i->checkparola(parola) == 1)
             {
                 return i;
             }
@@ -98,12 +224,12 @@ void inregistrare(list<utilizator*>& utilizatori)
     } while (cnp_unic(utilizatori,cnp)==1);
 
     do {
-        cout << "\nintrodu email unic"; cin >> email;
+        cout << "\nintrodu email unic: "; cin >> email;
         if(email_unic(utilizatori,email)==1)
             cout << "\nemail-ul introdus nu este unic, va rugam reintroduceti un email unic";
     } while (email_unic(utilizatori, email) == 1);
     do {
-        cout << "\n introdu parola: "; cin >> parola;
+        cout << "\nintrodu parola: "; cin >> parola;
         if (parola.length() < 3)
             cout << "\nparola introdusa are mai putin de 3 caractere";
     } while (parola.length() < 3);
@@ -114,52 +240,184 @@ void afisare_utilizatori(list<utilizator*>utilizatori)
     for (auto i : utilizatori)
         i->afisare_utilizator();
 }
-void conectare(list<utilizator*>&utilizatori)
+void resetare_parola(list<utilizator*>utilizatori)
+{
+    string email,parola;
+    cout << "\nIntrodu  adresa de email pentru care vrei sa resetezi parola: "; cin >> email;
+    if (email_unic(utilizatori, email) == 0)
+        cout << "\nNu exista cont creat cu acest email";
+    else {
+        for (auto user : utilizatori)
+        {
+            if (user->getemail() == email)
+            {
+                cout << "\nintrodu noua parola: "; cin >> parola;
+                user->reset_parola(parola);
+                break;
+            }
+        }
+    }
+    
+}
+void meniu_autentificat(utilizator* utilizator,list<carte*>carti) //zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+{
+    string titlu;
+    int opt, opt2;
+    do {
+        cout << "\nAlegeti o optiune:\n1. Achizitie/Imprumut\n2. Istoric\n3. Contact\n4.Inapoi\n"; cin >> opt;
+        switch (opt)
+        {
+        case 1:
+            for (auto i : carti)
+            {
+                i->afisare_carte();
+                cout << "\n";
+            }
+            do{
+            cout << "\nalegeti optiunea:\n1.Achizitioneaza\n2.Imprumut\n3.Inapoi\n"; cin >> opt2;
+            switch (opt2)
+            {
+            case 1:
+                cout << "Introdu titlul cartii pe care vrei sa o cumperi: "; cin >> titlu;
+                for (auto i : carti)
+                {
+                    if (i->gettitlu() == titlu)
+                        if (i->getstoc() == 0)
+                            cout << "\nStoc epuizat!";
+                        else
+                        {
+                            i->diminuare_stoc();
+                            utilizator->crestecheltuiala(i->getpret());
+                            utilizator->adaugacarteinistoric(i);/// ??
+                            break;
+                        }
+                }
+
+                break;
+
+            case 2:
+                cout << "Introdu titlul cartii pe care vrei sa o imprumuti: "; cin >> titlu;
+                for (auto i : carti)
+                {
+                    if (i->checkimprumut() == true)
+
+                        cout << "\Cartea nu e disponibila!";
+                    else
+                    {
+                        i->seteaza_imprumut();
+                    }
+                    break;
+                }
+                break;
+            case 3:
+                break;
+            };
+            }while (opt2 != 3);
+            break;
+        case 2:
+            utilizator->afisareistoric();
+            break;
+        case 3:
+
+            break;
+        case 4:
+            break;
+
+
+        };
+    } while (opt!=4);
+}
+void conectare(list<utilizator*>&utilizatori,list<carte*>carti)
 {
     utilizator* u;
     int opt;
-    cout << "\nAlegeti o optiune:\n1.Autentificare\n2.Inregistrare\n3.Resetare parola\n4.Inapoi\n";
+    cout << "\nAlegeti o optiune:\n1.Inregistrare\n2.Autentificare\n3.Resetare parola\n4.Inapoi\n";
+
     cin >> opt;
     switch (opt)
     {
     case 1:
-        if (autentificare(utilizatori) != 0)
-            cout << "autentificare cu succes";
-            u=autentificare(utilizatori);
-
-        break;
-    case 2:
         inregistrare(utilizatori);
         break;
+    case 2:
+            u = autentificare(utilizatori);
+        if (u != 0)
+        {
+            cout << "autentificare cu succes\n";
+        meniu_autentificat(u,carti);
+        }
+        else {
+            cout << "Nu ati introdus datele corect, va rog sa verificati sau sa va creati un cont in caz ca nu aveti";
+        }
+        break;
     case 3:
-
+        resetare_parola(utilizatori);
         break;
     case 4:
         break;
     }
 
 }
-void pagina_autentificat(utilizator* utilizator) //zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+void citire_carti(list<carte*>& carti)
 {
+    list<string>autori;
+    string titlu, editura, autor, tip;
+    int nr_pag, an_pub;
+    int stoc;
+    float pret;
+    ifstream fin("carti.txt");
+    while (fin >> tip)
+    {
+        if (tip == "fictiune")
+            fin >> nr_pag;
+        else if (tip == "nonfictiune")
+            fin >> an_pub;
+        fin >> titlu >> editura >> pret >> stoc;
+        autori.clear();
+        while (fin >> autor && autor != ".")
+            autori.push_back(autor);
+
+        if (tip == "fictiune")
+            carti.push_back(new carte_fictiune(titlu, autori, editura, pret, stoc, nr_pag));
+        else if (tip == "nonfictiune")
+            carti.push_back(new carte_nonfictiune(titlu, autori, editura, pret, stoc, an_pub));
+    }
+}
+
+void contact()
+{
+    int opt;
+    cout << "\nBookstoreApp.\n\nIn cazul in care inalniti dificultati echipa de suport va sta la dispozitie.\nTot ce trebuie sa faceti este sa completati urmatorul formular in care trebuie descrisa problema\nAlegeti o optiune:\n1.Trimitere sesizare\n2.Inapoi\n";
+    cin >> opt;
+    switch (opt)
+    {
+    case 1:
+
+        break;
+    case 2:
+        break;
+    };
 
 }
 int main()
 {
     list<utilizator*>utilizatori;
-    int opt;
-    
+    list<carte*>carti;
+    int opt,opt2;
+    citire_carti(carti);
 
     do
     {
-        cout << "BookstoreApp.\n\nAlege o optiune: \n1)Conectare\n2)Contact\n3)afisare useri";
+        cout << "\nBookstoreApp.\n\nAlege o optiune: \n1)Conectare\n2)Contact\n3)afisare utilizatori inregistrati\n";
         cin >> opt;
         switch (opt)
         {
         case 1:
-            conectare(utilizatori);
+            conectare(utilizatori,carti);
             break;
         case 2:
-            //contact
+            contact();
+            
             break;
         case 3:
             afisare_utilizatori(utilizatori);
